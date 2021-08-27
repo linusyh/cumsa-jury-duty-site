3d2607825b0a154df61847fa5afd9a234b6fa445
linus.leong.yh@gmail.com <linus.leong.yh@gmail.com>
2021-08-27 16:55:14 +0800

    mobile fix

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Jury Duty Game Evidence Locker',
+  serviceName: 'Jury Duty Evidence Locker',
 
   // Default port that prototype runs on
   port: '3000',
